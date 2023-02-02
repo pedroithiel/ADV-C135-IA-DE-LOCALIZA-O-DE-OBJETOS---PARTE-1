016524efdad5cf5f4dcdb051776ce97feb6e9892
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2023-02-02 15:14:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-function preload() {
+var objetoDeterctor;
+var carregar = false;
 
-}
 
 function setup() {
     canvas = createCanvas(500, 500)
     canvas.center()
+    video = createCapture(500,500)
+    video.hide()
+   
+}
+function iniciar() {
+
+    objetoDeterctor = ml5.objectDetector("cocossd", modelLoad)
+    document.getElementById("Status").innerHTML = "Status: objeto detectado"
 }
 
 function draw() {
-    
+image(video,0,0,500,500)    
+}
+
+function modelLoad() {
+console.log("model load!")
+carregar = true;
 }
